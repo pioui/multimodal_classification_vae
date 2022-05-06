d40624daa0b6bf98dd1e16e14a5d6cf17ed8b5d2
Piyi Lozou <plo026@tromso-stud-377.wifi.uit.no>
2022-05-06 10:41:44 +0200

    colorbar fix

--- scripts/visualize_results_plain.py
+++ scripts/visualize_results_plain.py
@@ -18,7 +18,6 @@ print(os.listdir('outputs/'))
 for project_name in os.listdir('outputs/'):
     if project_name == 'trento':
         print(project_name)
-        print("here1")
         dataset = 'trento'
         from trento_config import (
             labels,
@@ -80,76 +79,59 @@ for project_name in os.listdir('outputs/'):
         )
 
     else:
-        print(project_name)
-        print("here2")
         continue
 
     # Accuracies
     print(project_name, dataset)
 
-    with open(f"{outputs_dir}{PROJECT_NAME}.pkl", 'rb') as f:
-        data = pickle.load(f)
-    print(data[['MODEL_NAME','N_LATENT', 'encoder_type','LR','N_EPOCHS', 'M_ACCURACY','M_BALANCED_ACCURACY']])
-    data_csv = data[['MODEL_NAME','N_LATENT', 'encoder_type','M_ACCURACY','M_BALANCED_ACCURACY']]
-    data_csv.to_csv(f'{outputs_dir}/{PROJECT_NAME}_test_accuracies.csv')
-    data_dict = data.to_dict()        
-        
-    for i in range(len(data_dict['LR'])):
-        encoder_type = data_dict["encoder_type"][i]
-        model_name =  data_dict["MODEL_NAME"][i]
-        m_confusion_matrix = data_dict["CONFUSION_MATRIX"][i]
-        m_confusion_matrix = np.around(m_confusion_matrix.astype('float') / m_confusion_matrix.sum(axis=1)[:, np.newaxis], decimals=2)
-        train_loss = data_dict["train_LOSS"][i]
-        test_loss = data_dict["test_LOSS"][i]
+    if os.path.exists(f"{outputs_dir}{PROJECT_NAME}.pkl"):
 
-        # Train-Test Loss
-        plt.figure(dpi=500)
-        plt.plot(train_loss, color="red", label = 'Train Loss')
-        plt.plot(test_loss, color="blue", label='Test Loss')
-        plt.xlabel("Epochs")
-        plt.grid()
-        plt.legend()
-        plt.savefig(f"{images_dir}{project_name}_{model_name}_{encoder_type}_LOSS.png", pad_inches=0.2, bbox_inches='tight')
-        
-        plt.figure(dpi=500)
-        plt.matshow(m_confusion_matrix, cmap="YlGn")
-        plt.xlabel("True Labels")
-        plt.xticks(np.arange(0,N_LABELS,1), range(1,len(labels)))
-        plt.ylabel("Predicted Labels")
-        plt.yticks(np.arange(0,N_LABELS,1), range(1,len(labels)))
-        for k in range (len(m_confusion_matrix)):
-            for l in range(len(m_confusion_matrix[k])):
-                plt.text(k,l,str(m_confusion_matrix[k][l]), va='center', ha='center', fontsize='xx-small')
-        plt.savefig(f"{images_dir}{project_name}_{model_name}_{encoder_type}_test_CONFUSION_MATRIX.png",bbox_inches='tight', pad_inches=0.2, dpi=500)
-        np.savetxt(f"{outputs_dir}{project_name}_{model_name}_{encoder_type}_test_CONFUSION_MATRIX.csv", m_confusion_matrix.astype(int), delimiter=',')
+        with open(f"{outputs_dir}{PROJECT_NAME}.pkl", 'rb') as f:
+            data = pickle.load(f)
+        print(data[['MODEL_NAME','N_LATENT', 'encoder_type','LR','N_EPOCHS', 'M_ACCURACY','M_BALANCED_ACCURACY']])
+        data_csv = data[['MODEL_NAME','N_LATENT', 'encoder_type','M_ACCURACY','M_BALANCED_ACCURACY']]
+        data_csv.to_csv(f'{outputs_dir}/{PROJECT_NAME}_test_accuracies.csv')
+        data_dict = data.to_dict()        
+            
+        for i in range(len(data_dict['LR'])):
+            encoder_type = data_dict["encoder_type"][i]
+            model_name =  data_dict["MODEL_NAME"][i]
+            m_confusion_matrix = data_dict["CONFUSION_MATRIX"][i]
+            m_confusion_matrix = np.around(m_confusion_matrix.astype('float') / m_confusion_matrix.sum(axis=1)[:, np.newaxis], decimals=2)
+            train_loss = data_dict["train_LOSS"][i]
+            test_loss = data_dict["test_LOSS"][i]
+
+            # Train-Test Loss
+            plt.figure(dpi=500)
+            plt.plot(train_loss, color="red", label = 'Train Loss')
+            plt.plot(test_loss, color="blue", label='Test Loss')
+            plt.xlabel("Epochs")
+            plt.grid()
+            plt.legend()
+            plt.savefig(f"{images_dir}{project_name}_{model_name}_{encoder_type}_LOSS.png", pad_inches=0.2, bbox_inches='tight')
+            
+            plt.figure(dpi=500)
+            plt.matshow(m_confusion_matrix, cmap="YlGn")
+            plt.xlabel("True Labels")
+            plt.xticks(np.arange(0,N_LABELS,1), range(1,len(labels)))
+            plt.ylabel("Predicted Labels")
+            plt.yticks(np.arange(0,N_LABELS,1), range(1,len(labels)))
+            for k in range (len(m_confusion_matrix)):
+                for l in range(len(m_confusion_matrix[k])):
+                    plt.text(k,l,str(m_confusion_matrix[k][l]), va='center', ha='center', fontsize='xx-small')
+            plt.savefig(f"{images_dir}{project_name}_{model_name}_{encoder_type}_test_CONFUSION_MATRIX.png",bbox_inches='tight', pad_inches=0.2, dpi=500)
+            np.savetxt(f"{outputs_dir}{project_name}_{model_name}_{encoder_type}_test_CONFUSION_MATRIX.csv", m_confusion_matrix.astype(int), delimiter=',')
+    else:
+        print("No .pkl file")
 
     if dataset == "trento":
         y = np.array(io.loadmat(data_dir+"TNsecSUBS_Test.mat")["TNsecSUBS_Test"]) # [166,600] 0 to 6
         y_true = y.reshape(-1)
-        image_hyper = torch.tensor(tifffile.imread(data_dir+"hyper_Italy.tif")) # [63,166,600]
-        x_all = image_hyper.reshape(len(image_hyper),-1)
-        x_all = torch.transpose(x_all, 1,0)
-        x_min = x_all.min(dim=0)[0] # [65]
-        x_max = x_all.max(dim=0)[0] # [65]
-        x_all = (x_all- x_min)/(x_max-x_min)
-        hyperrgb = np.zeros((166,600,3))
-        hyperrgb[:,:,0] = x_all[:,40].reshape(166,600)+0.05
-        hyperrgb[:,:,1] = x_all[:,20].reshape(166,600)+0.1
-        hyperrgb[:,:,2] = x_all[:,0].reshape(166,600)+0.2
     
     if dataset == "houston":
         y = np.array(tifffile.imread(data_dir+"houston_gt.tif"), dtype = np.int64) # [1202,4768]
         y_true = y.reshape(-1)
-        image_hyper = torch.tensor(tifffile.imread(data_dir+"houston_hyper.tif")) # [50,1202,4768]
-        x_all = image_hyper.reshape(len(image_hyper),-1)
-        x_all = torch.transpose(x_all, 1,0)
-        x_min = x_all.min(dim=0)[0] # [57]
-        x_max = x_all.max(dim=0)[0] # [57]
-        x_all = (x_all- x_min)/(x_max-x_min)
-        hyperrgb = np.zeros((1202,4768,3))
-        hyperrgb[:,:,0] = x_all[:,40].reshape(1202,4768)+0.05
-        hyperrgb[:,:,1] = x_all[:,20].reshape(1202,4768)+0.1
-        hyperrgb[:,:,2] = x_all[:,0].reshape(1202,4768)+0.2
+
 
     acc_dict = []
     for file in os.listdir(os.path.join(outputs_dir)):
@@ -170,7 +152,7 @@ for project_name in os.listdir('outputs/'):
             plt.figure(dpi=500)
             plt.imshow((1-y_pred_max_prob).reshape(SHAPE), cmap='cool', vmin=0, vmax=1)
             plt.axis('off')
-            cbar = plt.colorbar(location='top', vmin=0, vmax=1)
+            cbar = plt.colorbar(location='top')
             cbar.ax.tick_params(labelsize =8 )
             plt.savefig(f"{images_dir}{model_name}_UNCERTAINTY.png",bbox_inches='tight', pad_inches=0.1 ,dpi=500)
 

c8ce8bcb111e5930e86c4806fe0ef9c9812793e0
pioui <piyilozou@gmail.com>
2022-05-16 14:26:01 +0200

    houston_patch

--- scripts/houston_patch_config.py
+++ scripts/houston_patch_config.py
@@ -33,18 +33,18 @@ if not os.path.exists(outputs_dir):
 if not os.path.exists(images_dir):
     os.makedirs(images_dir)
 
-N_EPOCHS = 1
+N_EPOCHS = 1000
 LR = 1e-4
 N_PARTICULES = 30
 N_HIDDEN = 128
 N_EXPERIMENTS = 1
 N_INPUT = 57
-PATCH_SIZE = 250
+PATCH_SIZE = 3
 N_LABELS = 20
 SHAPE = (1202,4768)
 CLASSIFICATION_RATIO = 50.0
 N_EVAL_SAMPLES = 25
-BATCH_SIZE = 1024
+BATCH_SIZE = 128
 PROJECT_NAME = "houston_patch"
 SAMPLES_PER_CLASS = 10
 
@@ -80,20 +80,20 @@ SCENARIOS = [  # WAKE updates
         loss_wvar="ELBO",
         reparam_latent=True,
         counts=None,
-        n_latent = 15,
-        model_name="EncoderB6_L15_VAE",
+        n_latent = 30,
+        model_name="EncoderB6_L30_VAE",
         encoder_z1=nn.ModuleDict(
             {
                 "default": EncoderB6( 
                 n_input=N_INPUT,
-                n_output=15,
+                n_output=30,
                 n_hidden=128,
                 dropout_rate=0,
                 do_batch_norm=False,
             )}
         ),
         x_decoder=BernoulliDecoderA6( 
-                n_input=15,
+                n_input=30,
                 n_output=N_INPUT,
                 dropout_rate=0,
                 do_batch_norm=False,
